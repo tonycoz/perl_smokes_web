37a13b7824e862727d7cb3ef7712013ceb1d1951
Tony Cook <tony@develop-help.com>
2023-09-23 03:51:58 +0000

    prevent warnings about autoincrement columne for GitBranch and GitCommit

--- lib/SmokeReports/Schema/Result/GitBranch.pm
+++ lib/SmokeReports/Schema/Result/GitBranch.pm
@@ -4,7 +4,13 @@ use parent qw/DBIx::Class::Core/;
 use SmokeReports::Sensible;
 
 __PACKAGE__->table('git_branches');
-__PACKAGE__->add_columns(qw/id name/);
+__PACKAGE__->add_columns(
+    id => {
+	is_auto_increment => 1,
+	data_type => "integer",
+    },
+    qw/name/
+    );
 __PACKAGE__->set_primary_key("id");
 
 1;
