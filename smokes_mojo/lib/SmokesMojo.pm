b3bcce7c330c28656e33ba817d1864c64cb36378
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2024-10-30 15:57:54 +0100

    Proof of concept /api/report in perl5.develop-help.com

--- smokes_mojo/lib/SmokesMojo.pm
+++ smokes_mojo/lib/SmokesMojo.pm
@@ -45,6 +45,7 @@ sub startup ($self) {
     $r->get('/api/reports_from_id/<id:num>')->to("api#reports_from_id");
     $r->get('/api/report_data/<id:num>')->to("api#report_data");
     $r->post('/api/postreport/post')->to("api#post_report");
+    $r->post('/api/report')->to("api#post_coresmokedb_report");
     $r->get('/api/nntp_from_id/<id:num>')->to("api#nntp_from_id");
     $r->get('/api/nntp_data/<id:num>')->to("api#nntp_data");
 }
