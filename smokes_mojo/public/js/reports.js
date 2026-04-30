fdb8060bc0cbf28d452b3eff7805df75b9d3e3a4
Tony Cook <tony@develop-help.com>
2026-04-30 11:27:07 +0000

    add a simple UI to copy the commit sha to the clipboard

--- smokes_mojo/public/js/reports.js
+++ smokes_mojo/public/js/reports.js
@@ -98,4 +98,16 @@ window.addEventListener("load", (event) => {
 	    s.form.submit();
 	}
     });
+    var copy_buttons = document.getElementsByClassName("copycommit");
+    for (var button of copy_buttons) {
+	button.addEventListener("click", (event) => {
+	    var target = event.currentTarget;
+	    var sha = target.dataset["sha"];
+	    navigator.clipboard.writeText(sha);
+	    target.classList.add("highlight");
+	    setTimeout(() => {
+		target.classList.remove("highlight");
+	    }, 500);
+	});
+    }
 });
